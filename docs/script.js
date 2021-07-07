dc29c12cc388ceffbe8c98bb26128cadd020ff52
kewlamogh <amogh_sarma@outlook.com>
2021-07-07 12:56:31 -0700

    added cannon tilt & id for HTML Canvas elem

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,11 @@
 // ok thx to @Blindman on stackoverflow for dis
 
+let cannon = document.createElement('img');
+let mouse = {x: undefined, y: undefined}
+let ctx = document.getElementById('canv').getContext('2d');
+cannon.src = 'imgs/cannon.jpg';
+
+document.onmousemove = function (event) {mouse.x = event.clientX; mouse.y = event.clientY;};
 
 function drawImageLookat(img, x, y, lookx, looky){
     ctx.setTransform(1, 0, 0, 1, x, y);  // set scale and origin
@@ -7,3 +13,14 @@ function drawImageLookat(img, x, y, lookx, looky){
     ctx.drawImage(img,-img.width / 2, -img.height / 2); // draw image
     ctx.setTransform(1, 0, 0, 1, 0, 0); // restore default not needed if you use setTransform for other rendering operations 
 }
+
+function render() {
+    ctx.clearRect(0, 0, 1500, 1500)
+    drawImageLookat(cannon, 0, 500, mouse.x, mouse.y);
+}
+
+function main() {
+    render();
+    requestAnimationFrame(main);
+}
+main();
