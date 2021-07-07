b0c95c96455e77fc08323a55ad4715a28e919b44
kewlamogh <amogh_sarma@outlook.com>
2021-07-07 16:38:43 -0700

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -11,16 +11,20 @@ cannon.src = 'cannon.jpg';
 
 document.onmousemove = function (event) { mouse.x = event.clientX; mouse.y = event.clientY;};
 document.onclick = async function (event){
-    b = {x: 50, y: 430};
-    bdir = dir;
-    b.x += Math.sin(bdir) * 50;
-    b.y += Math.cos(bdir) * 50;
-    let prgs = {x: Math.sin(bdir) * 22, y:Math.cos(bdir) * 22}
-    for (var i = 0; i <= 30; i++) {
-        b.x -= prgs.x;
-        b.y -= prgs.y;
-        prgs.y -= 1;
-        await new Promise(resolve => setTimeout(resolve, 10))
+    if (!isFiring) {
+        isFiring = true;
+        b = {x: 50, y: 430};
+        bdir = dir;
+        b.x += Math.sin(bdir) * 50;
+        b.y -= Math.cos(bdir) * 50;
+        let prgs = {x: Math.sin(bdir) * 22, y:Math.cos(bdir) * 22}
+        for (var i = 0; i <= 150; i++) {
+            b.x -= prgs.x;
+            b.y -= prgs.y;
+            prgs.y -= 1;
+            await new Promise(resolve => setTimeout(resolve, 10))
+        }
+        isFiring = false;
     }
 }
 
