d439177d7b2e62e0ce8b9b0894e7ae1e4592fc33
kewlamogh <amogh_sarma@outlook.com>
2021-07-07 16:01:50 -0700

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,7 @@
 // ok thx to @Blindman on stackoverflow for the drawimagelookat func
 
+let isFiring = false;
+let b = {x: 0, y:0};
 let cannon = document.createElement('img');
 let mouse = {x: undefined, y: undefined}
 let ctx = document.getElementById('canv').getContext('2d');
@@ -7,18 +9,29 @@ cannon.src = 'cannon.jpg';
 
 document.onmousemove = function (event) {mouse.x = event.clientX; mouse.y = event.clientY;};
 
+document.onclick = async function (event){
+    b = {x: 50, y: 125};
+    for (var i = 0; i <= 60; i++) {
+        b.y -= 5;
+        await new Promise(resolve => setTimeout(resolve, 1))
+    }
+}
+
 function drawImageLookat(img, x, y, lookx, looky){
     ctx.setTransform(1, 0, 0, 1, x, y);  // set scale and origin
     ctx.rotate(Math.atan2(looky - y, lookx - x)); // set angle
     ctx.drawImage(img,-img.width / 2, -img.height / 2); // draw image
     ctx.setTransform(1, 0, 0, 1, 0, 0); // restore default not needed if you use setTransform for other rendering operations 
+    let my=document.createElement('img');
+    my.src = 'human (2).jpg';
+    ctx.drawImage(my, b.x, b.y)
 }
 
 function render() {
     ctx.clearRect(0, 0, 1500, 1500);
     drawImageLookat(cannon, 50, 430, mouse.x, mouse.y);
 }
-function main() {
+async function main() {
     render();
     requestAnimationFrame(main);
 }
