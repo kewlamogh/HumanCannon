c607e787482a25ca5ad66ce05432d5395c88c960
kewlamogh <amogh_sarma@outlook.com>
2021-07-07 12:44:11 -0700

    three changed files

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,9 @@
+// ok thx to @Blindman on stackoverflow for dis
 
+
+function drawImageLookat(img, x, y, lookx, looky){
+    ctx.setTransform(1, 0, 0, 1, x, y);  // set scale and origin
+    ctx.rotate(Math.atan2(looky - y, lookx - x)); // set angle
+    ctx.drawImage(img,-img.width / 2, -img.height / 2); // draw image
+    ctx.setTransform(1, 0, 0, 1, 0, 0); // restore default not needed if you use setTransform for other rendering operations 
+}
