53f01d66596f6bae250cab08511843b9b271a087
kewlamogh <amogh_sarma@outlook.com>
2021-07-07 20:40:32 -0700

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -16,7 +16,7 @@ let cannon = genImg('cannon.jpg'); //the cannon's HTMLImgElement
 let mouse = {x: undefined, y: undefined}; //mouse pos
 let canvas = document.getElementById('canv'); //canvas
 let ctx = canvas.getContext('2d'); //context
-let humans = ['docs/imgs/human2.jpg', 'docs/imgs/human.jpg', 'docs/imgs/human3.jpg']//humans
+let humans = ['/imgs/human2.jpg', '/imgs/human.jpg', '/imgs/human3.jpg']//humans
 
 
 canvas.onmousemove = function (event) {mouse.x = event.clientX; mouse.y = event.clientY;}; /**
